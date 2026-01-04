2b2a33acfabdf74265cdf93a3323dcb29d548fea
Nicolas Gasco <nicolas.gasco92@gmail.com>
2026-01-04 19:47:27 +0100

    Small UI improvements on HomePage screen

--- interface/src/hooks/useFetchBreaks.tsx
+++ interface/src/hooks/useFetchBreaks.tsx
@@ -9,6 +9,14 @@ export function useFetchBreaks(): {
     useEffect(() => {
         async function fetchBreaks() {
             const response = await fetch('http://localhost:8080/breaks');
+
+            if (!response.ok) {
+                console.error('Failed to fetch breaks:', response.statusText);
+                return {
+                    breaks: []
+                }
+            }
+
             const {breaks}: {
                 breaks: BreakSummary[];
                 count: number;
--- interface/src/pages/HomePage.tsx
+++ interface/src/pages/HomePage.tsx
@@ -5,10 +5,13 @@ export function HomePage() {
 
     return (
         <>
-            <h1 className="mb-2">Welcome to SurfShare</h1>
-            <p className="mb-6">Start by choosing one of the surf breaks below</p>
+            <h1 className="mb-4">Welcome to SurfShare</h1>
+            <p className="mb-6">Start by choosing one of the surf breaks below to find more about them.</p>
 
-            <select name="breaks" className="text-md bg-white text-black p-2 rounded shadow-lg font-medium">
+            <select name="breaks"
+                    className="text-md bg-white disabled:opacity-50 disabled:cursor-not-allowed text-black p-2 rounded shadow-lg font-medium min-w-[200px] text-center"
+                    disabled={breaks.length === 0}>
+                <option selected disabled>Choose a break</option>
                 {
                     breaks.map((breakSummary) => (
                         <option key={breakSummary.id} value={breakSummary.slug}>

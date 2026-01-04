5abfbea762ccd1e015d1e747d111e5779938433e
Nicolas Gasco <nicolas.gasco92@gmail.com>
2026-01-04 16:30:20 +0100

    Serve static page on API root

--- app/main.go
+++ app/main.go
@@ -10,26 +10,22 @@ import (
 func main() {
 	mux := http.NewServeMux()
 
-	mux.Handle("/", &homeHandler{})
+	mux.HandleFunc("/", handleRoot)
 
 	// Breaks
-	mux.Handle("/breaks", &BreaksHandler{})
+	mux.HandleFunc("GET /breaks", handleBreaks)
 
 	port := os.Getenv("PORT")
-	fmt.Printf("Server is running on port %s\n", port)
+	fmt.Printf("Server is listening to port %s\n", port)
 	http.ListenAndServe(":"+port, mux)
 }
 
-type homeHandler struct{}
-
-func (h *homeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("This is my home page"))
-
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+	filePath := "static/app.html"
+	http.ServeFile(w, r, filePath)
 }
 
-type BreaksHandler struct{}
-
-func (h *BreaksHandler) ListBreaks(w http.ResponseWriter, r *http.Request) {
+func handleBreaks(w http.ResponseWriter, r *http.Request) {
 	breaks := []string{"La Arena", "Sopelana"}
 
 	resp := struct {
@@ -43,12 +39,5 @@ func (h *BreaksHandler) ListBreaks(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	if err := json.NewEncoder(w).Encode(resp); err != nil {
 		http.Error(w, "failed to encode response", http.StatusInternalServerError)
-	}
-}
-
-func (h *BreaksHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch {
-	case r.Method == http.MethodGet:
-		h.ListBreaks(w, r)
 	}
 }

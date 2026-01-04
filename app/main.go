ea9dd469461f82599224264d039b572a4fc61858
Nicolas Gasco <nicolas.gasco92@gmail.com>
2026-01-04 15:02:10 +0100

    Initialize REST api with /breaks endpoint

--- app/main.go
+++ app/main.go
@@ -1,7 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	mux := http.NewServeMux()
+
+	mux.Handle("/", &homeHandler{})
+
+	// Breaks
+	mux.Handle("/breaks", &BreaksHandler{})
+
+	port := os.Getenv("PORT")
+	fmt.Printf("Server is running on port %s\n", port)
+	http.ListenAndServe(":"+port, mux)
+}
+
+type homeHandler struct{}
+
+func (h *homeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("This is my home page"))
+
+}
+
+type BreaksHandler struct{}
+
+func (h *BreaksHandler) ListBreaks(w http.ResponseWriter, r *http.Request) {
+	breaks := []string{"La Arena", "Sopelana"}
+
+	resp := struct {
+		Count  int      `json:"count"`
+		Breaks []string `json:"breaks"`
+	}{
+		Count:  len(breaks),
+		Breaks: breaks,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(resp); err != nil {
+		http.Error(w, "failed to encode response", http.StatusInternalServerError)
+	}
+}
+
+func (h *BreaksHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch {
+	case r.Method == http.MethodGet:
+		h.ListBreaks(w, r)
+	}
 }

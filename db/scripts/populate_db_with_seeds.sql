b2564e77ad73df43740040286286268f81c048a4
Nicolas Gasco <nicolas.gasco92@gmail.com>
2026-01-19 22:47:18 +0100

    Format file

--- db/scripts/populate_db_with_seeds.sql
+++ db/scripts/populate_db_with_seeds.sql
@@ -37,25 +37,12 @@ BEGIN;
 
 CREATE TABLE IF NOT EXISTS app.users
 (
-    id
-    UUID
-    PRIMARY
-    KEY
-    DEFAULT
-    gen_random_uuid
-(
-),
-    username VARCHAR
-(
-    50
-) NOT NULL UNIQUE,
-    email VARCHAR
-(
-    100
-) NOT NULL UNIQUE,
+    id       UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    username VARCHAR(50)  NOT NULL UNIQUE,
+    email    VARCHAR(100) NOT NULL UNIQUE,
     password TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    );
+);
 
 COMMIT;

2ae95717f4731bf6c0fa147f12934d1180f06bc8
Nicolas Gasco <nicolas.gasco92@gmail.com>
2026-02-02 19:25:52 +0100

    Refactor auth module to follow hexagonal architecture

--- app/internal/modules/auth/repository.go
+++ app/internal/modules/auth/repository.go
@@ -6,9 +6,22 @@ import (
 	"surf-share/app/internal/adapters"
 )
 
+type UserWithPassword struct {
+	ID       string `db:"id"`
+	Username string `db:"username"`
+	Email    string `db:"email"`
+	Password string `db:"password"`
+}
+
+type DbUser struct {
+	ID       string `db:"id"`
+	Username string `db:"username"`
+	Email    string `db:"email"`
+}
+
 type UserAuthRepository interface {
 	FindUserWithPasswordByEmail(ctx context.Context, email string) (*UserWithPassword, error)
-	CreateUser(ctx context.Context, username, email, hashedPassword string) (*User, error)
+	CreateUser(ctx context.Context, username, email, hashedPassword string) (*DbUser, error)
 }
 
 type Repository struct {
@@ -31,8 +44,8 @@ func (r *Repository) FindUserWithPasswordByEmail(ctx context.Context, email stri
 	return &userWithPassword, nil
 }
 
-func (r *Repository) CreateUser(ctx context.Context, username, email, hashedPassword string) (*User, error) {
-	var user User
+func (r *Repository) CreateUser(ctx context.Context, username, email, hashedPassword string) (*DbUser, error) {
+	var user DbUser
 
 	if err := r.db.CreateOne(ctx, &user,
 		"INSERT INTO app.users (username, email, password) VALUES ($1, $2, $3) RETURNING id, username, email",
--- app/internal/modules/auth/service.go
+++ app/internal/modules/auth/service.go
@@ -24,6 +24,17 @@ type authService struct {
 	tokenGenerator TokenGenerator
 }
 
+type User struct {
+	ID       string
+	Username string
+	Email    string
+}
+
+type AuthResult struct {
+	User  *User
+	Token string
+}
+
 func (s *authService) Register(ctx context.Context, username, email, password string) (*AuthResult, error) {
 	email = strings.ToLower(email)
 
@@ -37,13 +48,18 @@ func (s *authService) Register(ctx context.Context, username, email, password st
 		return nil, err
 	}
 
-	token, err := s.tokenGenerator.Generate(user)
+	userModel := &User{
+		ID:       user.ID,
+		Username: user.Username,
+		Email:    user.Email,
+	}
+	token, err := s.tokenGenerator.Generate(userModel)
 	if err != nil {
 		return nil, err
 	}
 
 	return &AuthResult{
-		User:  user,
+		User:  userModel,
 		Token: token,
 	}, nil
 }
